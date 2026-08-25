6b2d1d7cc0a03d22c8cdca50d64b1e1724471c10
Arena Deploy <arena-agent@users.noreply.github.com>
2026-08-25 16:25:27 +0000

    Deploy: Legacy Keepers static site 2026-08-25T16:25Z

--- _next/static/Ftb_iWKuhjdOe84icrXc3/_buildManifest.js
+++ _next/static/Ftb_iWKuhjdOe84icrXc3/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/legacy-keepers//_next/:path+",
-        "destination": "/legacy-keepers/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
